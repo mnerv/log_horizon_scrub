7413b87bfd1bebad09e477bc586c09b83d947356
mononerv <me@mononerv.dev>
2022-11-21 19:15:02 +0100

    Lab01: Just living in the Database Wow Wow

--- lab01/datas.sql
+++ lab01/datas.sql
@@ -7,29 +7,32 @@
  *
  * @copyright Copyright (c) 2022
  */
-INSERT INTO department
-VALUES
+INSERT INTO department VALUES
 (1, 'computer science'),
-(2, 'physics');
+(2, 'physics'         );
+(3, 'music'           );
 
-INSERT INTO teacher
-VALUES
+INSERT INTO teacher VALUES
 (138, 'Maria Foster', 1),
 (221, 'George Mardy', 2);
 
-INSERT INTO course
-VALUES
+INSERT INTO course VALUES
 ('CAP21', 'Programming'   , 138, 1),
 ('CAP33', 'Database'      , 138, 1),
 ('PIS32', 'Thermodynamics', 221, 2);
 
-INSERT INTO student
-VALUES
-(100, 'Michale', 'Robbin'   , 'Malmö', 1, 0987321),
-(101, 'Carlos' , 'Manuel'   ,  null  , 2, null),
-(102, 'Enrique', 'Sitaraman', 'Malmö', 1, 1234),
-(103, 'Joseph' , 'Dosni'    , 'Lund' , 2, 564738),
-(104, 'Mario'  , 'Robbin'   ,  null  , 1, null);
+INSERT INTO student VALUES
+(100, 'Michale' , 'Robbin'   , 'Malmö', 1, '0731298058'),
+(101, 'Carlos'  , 'Manuel'   ,  null  , 2, null        ),
+(102, 'Enrique' , 'Sitaraman', 'Malmö', 1, '0731298959'),
+(103, 'Joseph'  , 'Dosni'    , 'Lund' , 2, '0731298957'),
+(104, 'Mario'   , 'Robbin'   , 'Tokyo', 1, null        );
+(105, 'Tokyo'   , 'Tanaka'   , 'Tokyo', 3, null        );
+(106, 'Jean-Ken', 'Johanny'  , 'Tokyo', 3, null        );
+(107, 'Kamikaze', 'Boy'      , 'Tokyo', 3, null        );
+(108, 'DJ Santa', 'Monica'   , 'Tokyo', 3, null        );
+(109, 'Spear'   , 'Rib'      , 'Tokyo', 3, null        );
+(110, 'Takuma'  , 'Mitamura' , 'Kyoto', 3, null        );
 
 INSERT INTO registration VALUES
 (12, 102, 'PIS32', '2020-02-06', 'VG'),
