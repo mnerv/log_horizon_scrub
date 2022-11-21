e976db0e8330986d99ba4a99a65c0aeff44c8128
SpaceCrab <10452861+SpaceCrab@users.noreply.github.com>
2022-11-21 13:39:36 +0100

    created student_data.sql
    contains stundent data

---
+++ lab01/student_data.sql
@@ -0,0 +1,8 @@
+insert into student
+values
+(100, Michale, Robbin, Malmö,1,0987321),
+(101, Carlos, Manuel, null, 2, null),
+(102, Enrique, Sitaraman, Malmö,1,1234),
+(103,Joseph, Dosni, Lund, 2,564738),
+(104, Mario, Robbin, null, 1, null);
+
