522b9f0bdcc30a0b35c7abee1b5901c4030bc1f6
mononerv <me@mononerv.dev>
2022-11-24 22:49:14 +0100

    project: Brief description in file comment

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,7 +1,9 @@
 /**
  * @file   main.rs
  * @author Pratchaya Khansomboon (me@mononerv.dev)
- * @brief  Database front application.
+ * @author Eric Lundin
+ * @brief  Hope store - We sell hopes and dreams.
+ *         Database interface front application.
  * @date   2022-11-23
  *
  * @copyright Copyright (c) 2022
@@ -10,3 +12,4 @@
 fn main() {
     println!("Hello, World!");
 }
+
