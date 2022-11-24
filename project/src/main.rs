93002dba15cbc46147e2620437d558a1fb7a18da
mononerv <me@mononerv.dev>
2022-11-24 11:40:50 +0100

    project: ER-diagram and rust project creation
    First draft of our database design in an Entity-Relational Diagram.

---
+++ project/src/main.rs
@@ -0,0 +1,12 @@
+/**
+ * @file   main.rs
+ * @author Pratchaya Khansomboon (me@mononerv.dev)
+ * @brief  Database front application.
+ * @date   2022-11-23
+ *
+ * @copyright Copyright (c) 2022
+ */
+
+fn main() {
+    println!("Hello, World!");
+}
