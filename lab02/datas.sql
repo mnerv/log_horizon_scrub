aa2f25c23c7399b75cf1e0f44411a31563aa4160
mononerv <me@mononerv.dev>
2022-12-03 15:47:43 +0100

    Lab02: Tasks 1 to 4 maybe completed???
    Copy the old tables and data creation from lab01 to lab02.

---
+++ lab02/datas.sql
@@ -0,0 +1,42 @@
+/*
+ * @file   datas.sql
+ * @author Pratchaya Khansomboon (me@mononerv.dev)
+ * @author Eric Lundin
+ * @brief  Insert data into tables in correct order.
+ * @date   2022-11-21
+ *
+ * @copyright Copyright (c) 2022
+ */
+INSERT INTO department VALUES
+(1, 'computer science'),
+(2, 'physics'         ),
+(3, 'music'           );
+
+INSERT INTO teacher VALUES
+(138, 'Maria Foster', 1),
+(221, 'George Mardy', 2);
+
+INSERT INTO course VALUES
+('CAP21', 'Programming'   , 138, 1),
+('CAP33', 'Database'      , 138, 1),
+('PIS32', 'Thermodynamics', 221, 2);
+
+INSERT INTO student VALUES
+(100, 'Michale' , 'Robbin'   , 'Malmö', 1, '0731298058'),
+(101, 'Carlos'  , 'Manuel'   ,  null  , 2, null        ),
+(102, 'Enrique' , 'Sitaraman', 'Malmö', 1, '0731298959'),
+(103, 'Joseph'  , 'Dosni'    , 'Lund' , 2, '0731298957'),
+(104, 'Mario'   , 'Robbin'   , 'Tokyo', 1, null        );
+(105, 'Tokyo'   , 'Tanaka'   , 'Tokyo', 3, null        );
+(106, 'Jean-Ken', 'Johanny'  , 'Tokyo', 3, null        );
+(107, 'Kamikaze', 'Boy'      , 'Tokyo', 3, null        );
+(108, 'DJ Santa', 'Monica'   , 'Tokyo', 3, null        );
+(109, 'Spear'   , 'Rib'      , 'Tokyo', 3, null        );
+(110, 'Takuma'  , 'Mitamura' , 'Kyoto', 3, null        );
+
+INSERT INTO registration VALUES
+(12, 102, 'PIS32', '2020-02-06', 'VG'),
+(13, 104, 'CAP21', '2020-02-06', 'G' ),
+(14, 100, 'CAP21', '2020-11-18', 'NS'),
+(15, 102, 'CAP21', '2020-11-18', 'NS'),
+(16, 103, 'PIS32', '2020-11-18', 'NS');
---
+++ lab02/tasks.sql
@@ -0,0 +1,65 @@
+/*
+ * @file   tasks.sql
+ * @author Pratchaya Khansomboon (me@mononerv.dev)
+ * @author Eric Lundin
+ * @brief  Lab01 tasks.
+ * @date   2022-12-03
+ *
+ * @copyright Copyright (c) 2022
+ */
+
+-- Join, Grouping and View
+
+-- 01. Show id and full name of the students and the code of their registered courses
+SELECT s.firstname, s.lastname, s.id, r.course_code
+FROM student AS s
+INNER JOIN registration AS r ON s.id = r.student_id;
+
+-- SELECT s.firstname, s.lastname, s.id, COUNT(r.course_code)
+-- FROM student AS s
+-- INNER JOIN registration AS r ON s.id = r.student_id
+-- GROUP BY s.firstname, s.lastname, s.id;
+
+-- 2. Show id and full name of the students and the name of their registered courses.
+SELECT s.firstname, s.lastname, s.id, c.name AS course_name
+FROM student AS s
+INNER JOIN registration AS r ON s.id = r.student_id
+INNER JOIN course AS c ON r.course_code = c.code;
+
+-- 3. Show id and full name of the students and name of their current/old teachers.
+SELECT s.firstname, s.lastname, s.id, t.fullname AS teacher_name
+FROM student AS s
+INNER JOIN registration AS r ON s.id = r.student_id
+INNER JOIN course AS c ON r.course_code = c.code
+INNER JOIN teacher AS t ON t.id = c.teacher_id;
+
+-- 4. Show id and full name of the students, and number of teachers they have had any
+--    course with them. Be careful that if student X is/was taking two courses which are
+--    given by the same teacher Y, that student has only one teacher.
+SELECT s.firstname, s.lastname, s.id, COUNT(c.teacher_id) AS number_of_teachers
+FROM student AS s
+INNER JOIN registration AS r ON s.id = r.student_id
+INNER JOIN course AS c on c.code = r.course_code
+INNER JOIN teacher AS t on t.id = c.teacher_id 
+GROUP BY s.firstname, s.lastname, s.id;
+
+-- 5. It is quite usual to make a report of the courses and the number of registered
+--    students on each course in each year. Thus, it is good to create a view for that.
+--    Please write a suitable sql command to create a view for that report. 
+
+-- 6. Show a list of course name and number of students registered for that course in
+--    year 2020. Is it better to use your created View in exercise 5 or writing a new Join
+--    command?
+
+-- Procedures
+
+-- 7. Here, we want you to write a store procedure to receive the first name and last
+--    name of a student as input parameters and show the department of the student.
+--    For example:
+
+-- 8. Alter your store procedure in exercise 7 to make it more flexible: To receive the
+--    first name, last name as input parameters, and show the department of all the
+--    students who have similar first name and last name. For example:
+
+-- 9. Please check and be sure if your store procedures work successfully by executing
+--    them!
