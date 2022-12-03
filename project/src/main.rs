37aa7f519ec670c675f366a7d7743291e387e92d
mononerv <me@mononerv.dev>
2022-12-03 14:56:55 +0100

    project: Connection between rust and postgres database
    Execute simple commands to create a table and insert data and query the
    actual values.

--- project/src/main.rs
+++ project/src/main.rs
@@ -8,8 +8,35 @@
  *
  * @copyright Copyright (c) 2022
  */
+use postgres::{Client, NoTls, Error};
 
-fn main() {
-    println!("Hello, World!");
+fn main() -> Result<(), Error> {
+    const HOST: &'static str = "localhost";
+    const USER: &'static str = "miku";
+    const PASSWORD: &'static str = "hatsune";
+    const DATABASE: &'static str = "mikudb";
+    let client_param = format!("host={} user={} password='{}' dbname={}", HOST, USER, PASSWORD, DATABASE);
+    let mut client = Client::connect(&client_param, NoTls)?;
+
+    // client.batch_execute("
+    //     CREATE TABLE person(
+    //         id INT NOT NULL UNIQUE,
+    //         name VARCHAR(30)
+    //     )
+    // ")?;
+
+    // client.batch_execute("
+    //     INSERT INTO person VALUES
+    //     (1, 'Eric'),
+    //     (2, 'Pratchaya');
+    // ")?;
+
+
+    for row in client.query("SELECT id, name FROM person", &[])? {
+        let id: i32 = row.get(0);
+        let name: &str = row.get(1);
+        println!("found person: {} {}", id, name);
+    }
+
+    Ok(())
 }
-
