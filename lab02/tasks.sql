29749839f03a04ab35accad660e79609ef70734e
mononerv <me@mononerv.dev>
2022-12-21 22:11:20 +0100

    lab02: Complete! Functions here we come.

--- lab02/tasks.sql
+++ lab02/tasks.sql
@@ -48,17 +48,36 @@ GROUP BY s.firstname, s.lastname, s.id;
 --    Please write a suitable sql command to create a view for that report. 
 
 CREATE VIEW course_enrollment AS 
-    SELECT c.name, COUNT(DISTINCT r.student_id ) AS enrollments
+    SELECT c.name, COUNT(DISTINCT r.student_id) AS enrollments
     FROM course AS c 
     INNER JOIN registration AS r ON c.code = r.course_code
     GROUP BY c.name;
+
+-- Use view with SELECT query
 SELECT * FROM course_enrollment;
 
 -- 6. Show a list of course name and number of students registered for that course in
 --    year 2020. Is it better to use your created View in exercise 5 or writing a new Join
 --    command?
 
+-- Fix view to include year in the data column
+CREATE VIEW course_enrollment_year AS 
+    SELECT c.name, COUNT(DISTINCT r.student_id) AS enrollments, EXTRACT(YEAR FROM r.date) as year
+    FROM course AS c 
+    INNER JOIN registration AS r ON c.code = r.course_code
+    GROUP BY c.name, year;
 
+-- Usage: SELECT * FROM course_enrollment_year WHERE year = '2020';
+
+SELECT c.name, COUNT(DISTINCT r.student_id) AS enrollments, EXTRACT(YEAR FROM r.date) as year
+FROM course AS c
+INNER JOIN registration AS r ON c.code = r.course_code
+WHERE EXTRACT(YEAR FROM r.date) = '2020'
+GROUP BY c.name, year;
+
+-- Answer: If we were to use the previous view in exercise 5 it would not work
+--         because it does not save the year in the data column. But a fix for it is by
+--         adding it in the column name and extract only the year and also group it together with the name.
 
 -- Procedures
 
@@ -66,9 +85,72 @@ SELECT * FROM course_enrollment;
 --    name of a student as input parameters and show the department of the student.
 --    For example:
 
+--    if first name = 'Carlos' and last name = 'Manuel', the procedure will return
+--    | Department_Name |
+--    | --------------- |
+--    | Math            |
+
+-- SELECT d.name AS department_name FROM student AS s
+-- INNER JOIN department AS d ON s.department_code = d.code
+-- WHERE s.firstname = 'Carlos' AND s.lastname = 'Manuel';
+
+CREATE OR REPLACE FUNCTION get_department(fname VARCHAR, lname VARCHAR)
+RETURNS TABLE (name VARCHAR)
+LANGUAGE plpgsql
+AS $$
+-- DECLARE
+-- VARIABLE DECLARATION
+BEGIN
+-- stored procedure body
+    RETURN query
+    SELECT d.name AS department_name FROM student AS s
+    INNER JOIN department AS d ON s.department_code = d.code
+    WHERE (s.firstname = fname) AND (s.lastname = lname);
+END; $$
+
+-- Note: Booby trap 'procedure' word confuses use that we need to create a procedure instead of function.
+
+-- Usage: select * from get_department('Carlos', 'Manuel')
+
 -- 8. Alter your store procedure in exercise 7 to make it more flexible: To receive the
 --    first name, last name as input parameters, and show the department of all the
 --    students who have similar first name and last name. For example:
 
+--    if first name = 'carl' and last name = '', the procedure will return
+--    | FirstName | LastName | DepartmentName |
+--    | --------- | -------- | -------------- |
+--    | Carlos    | Manuel   | Math           |
+
+--    if first name = '' and last name = '', the procedure will return
+--    | FirstName | LastName    | DepartmentName   |
+--    | --------- | ----------- | ---------------- |
+--    | Michale   | Robbin      | computer science |
+--    | Carlos    | Manuel      | Math             |
+--    | Enric     | Sitaraman   | computer science |
+--    | Joseph    | Dosni       | Math             |
+--    | Mario     | Robbin      | computer science |
+
+SELECT s.firstname, s.lastname, d.name AS department_name FROM student AS s
+INNER JOIN department AS d ON s.department_code = d.code
+WHERE LOWER(s.firstname) LIKE LOWER('%%') AND LOWER(s.lastname) LIKE LOWER('%rob%');
+
+CREATE OR REPLACE FUNCTION get_department(fname VARCHAR, lname VARCHAR)
+RETURNS TABLE (fistname VARCHAR, lastname VARCHAR, department_name VARCHAR)
+LANGUAGE plpgsql
+AS $$
+-- DECLARE
+-- VARIABLE DECLARATION
+BEGIN
+-- stored procedure body
+    RETURN query
+    SELECT s.firstname, s.lastname, d.name AS department_name FROM student AS s
+    INNER JOIN department AS d ON s.department_code = d.code
+    WHERE LOWER(s.firstname) LIKE LOWER('%'||fname||'%') AND LOWER(s.lastname) LIKE LOWER('%'||lname||'%');
+END; $$
+
+-- Usage: select * from get_department('', '');
+
 -- 9. Please check and be sure if your store procedures work successfully by executing
 --    them!
+
+-- They do smh and it's a store function not a fukken procedure
