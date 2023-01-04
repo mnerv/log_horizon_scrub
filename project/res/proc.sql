799288a83725d53f36bc4f42d4f99ddbd782c7f5
mononerv <me@mononerv.dev>
2023-01-04 17:16:02 +0100

    project: Procedure for creating customer and supplier
    Usage of procedure to create customer and supplier. This makes sure that
    the address is created for these which both the table depends on. If it
    fails to create then there won't be a free standing data row in the
    address table.

---
+++ project/res/proc.sql
@@ -0,0 +1,57 @@
+/*
+ * @file   proc.sql
+ * @author Pratchaya Khansomboon (me@mononerv.dev)
+ * @author Eric Lundin
+ * @brief  Database procedures for data insertions.
+ * @date   2023-01-04
+ *
+ * @copyright Copyright (c) 2023
+ */
+CREATE OR REPLACE PROCEDURE insert_customer(
+    first_name VARCHAR,
+    last_name VARCHAR,
+    email VARCHAR,
+    pass VARCHAR,
+    street VARCHAR,
+    postcode VARCHAR,
+    country VARCHAR,
+    tele VARCHAR
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    address_id INT;
+BEGIN
+    INSERT INTO address(street, postcode, city, country, telephone)
+    VALUES (street, postcode, city, country, tele)
+    RETURNING id INTO address_id;
+
+    INSERT INTO customer (address_id, first_name, last_name, email, password)
+    VALUES (address_id, first_name, last_name, email, pass);
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE insert_supplier(
+    admin_id INT,
+    name VARCHAR,
+    description VARCHAR,
+    orgnum VARCHAR,
+    street VARCHAR,
+    postcode VARCHAR,
+    city VARCHAR,
+    country VARCHAR,
+    tele VARCHAR
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    address_id INT;
+BEGIN
+    INSERT INTO address(street, postcode, city, country, telephone)
+    VALUES (street, postcode, city, country, tele)
+    RETURNING id INTO address_id;
+
+    INSERT INTO supplier (admin_id ,address_id, name, description, orgnum)
+    VALUES (admin_id, address_id, name, description, orgnum);
+END;
+$$;
